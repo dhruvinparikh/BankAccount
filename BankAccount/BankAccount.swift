dc778c4faf6cc3296d13413c5e3934945015311a
Dhruvin Parikh <n01150847@humbermail.ca>
2018-03-14 09:10:24 -0400

    First part finished/tested

--- BankAccount/BankAccount.swift
+++ BankAccount/BankAccount.swift
@@ -10,9 +10,103 @@ import Foundation
 
 public class BankAccount{
     //Account number, customer name, yearly interest rate, account balance as stored properties
-    public var account_number: String
-    public var customer_name: String
-    public var interest: Double
-    public var account_balance: Double
+    //public var account_number: String
+    //public var customer_name: String
+    //public var interest: Double
+    //public var account_balance: Double
     
+    //property observers for all properties
+    public var account_number: String = "" {
+        willSet{
+            print("Setting account number to \(newValue)")
+        }
+        didSet {
+            if(!(account_number != "")){
+                print("invalid account number, resetting to \(oldValue)")
+            }
+            }
+        }
+
+    public var customer_name: String = "" {
+        willSet{
+            print("Setting customer name to \(newValue)")
+        }
+        didSet {
+            if(!(customer_name != "")){
+                print("invalid customer name, resetting to \(oldValue)")
+            }
+        }
+    }
+    
+    public var interest: Double = 0.0 {
+        willSet{
+            print("Setting interest to \(newValue)")
+        }
+        didSet {
+            if(interest >= 0.0 && interest <= 2.0){
+            }
+            else{
+                print("invalid yearly interest rate, resetting to \(oldValue)")
+            }
+        }
+    }
+    
+    public var account_balance: Double = 10.0 {
+        willSet{
+            print("Setting account_balance to \(newValue)")
+        }
+        didSet {
+            if(!(account_balance >= 10.0)){
+                print("invalid acount balance, resetting to \(oldValue)")
+            }
+        }
+    }
+    
+//designated initializer: sets stored properties to specified values
+public init(account_number: String, customer_name: String, interest: Double, account_balance: Double) {
+    self.account_number=account_number
+    self.customer_name=customer_name
+    if(interest >= 0.0 || interest <= 2.0){
+    self.interest=interest
+    }
+    else{
+        self.interest=0.0
+    }
+    if(account_balance >= 10.00){
+    self.account_balance=account_balance
+    }
+    else{
+        self.account_balance=10.00
+    }
+}
+
+    public init(){
+        self.account_number="11111"
+        self.customer_name="xxx"
+        self.interest=0.0
+        self.account_balance=10.00
+    }
+    
+    //credit(amount:Double) which is used for depositing money
+    public func credit(amount:Double){
+        print("Depositing $\(amount)...")
+        self.account_balance = self.account_balance + amount
+    }
+    
+    //debit(amount:Double) which is used for withdrwng money
+    public func debit(amount:Double){
+        if(self.account_balance-amount >= 10.00){
+            print("Withdrawing $\(amount)...")
+            self.account_balance = self.account_balance - amount
+        }
+        else{
+            print("Transaction cancelled. You need minimum $10.00 balance")
+        }
+    }
+    
+//Define a description as computed property which display all the values of bank account object
+public var description: String {
+    return "Account number : " + String(self.account_number) + ", Customer name : " + String(self.customer_name) + ", Yearly interest rate : " + String(self.interest) + ", Account balance : " + String(self.account_balance)
+}
+
 }
--- BankAccount/main.swift
+++ BankAccount/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+let bankAccount1 = BankAccount(account_number: "12345", customer_name: "Dhruvin", interest: 11.0, account_balance: 2300.0)
+print(bankAccount1.description)
+bankAccount1.debit(amount: 500)
+print(bankAccount1.description)
+bankAccount1.credit(amount: 1000)
+
+print("--------------------------------------------------------")
+let bankAccount2 = BankAccount(account_number: "12356", customer_name: "Parikh", interest: 9.9, account_balance: 100)
+print(bankAccount2.description)
+bankAccount2.debit(amount: 100)
+print(bankAccount2.description)
+bankAccount2.credit(amount: 120)
+
+
 
