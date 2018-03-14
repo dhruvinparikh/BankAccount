bb5d595f88da65486f0d3028c6718e8d5c5e7450
Dhruvin Parikh <n01150847@humbermail.ca>
2018-03-14 08:14:22 -0400

    First commit

---
+++ BankAccount/BankAccount.swift
@@ -0,0 +1,18 @@
+//
+//  Bankaccount.swift
+//  BankAccount
+//
+//  Created by Parikh Dhruvin S. on 3/14/18.
+//  Copyright © 2018 Parikh Dhruvin S. All rights reserved.
+//
+
+import Foundation
+
+public class BankAccount{
+    //Account number, customer name, yearly interest rate, account balance as stored properties
+    public var account_number: String
+    public var customer_name: String
+    public var interest: Double
+    public var account_balance: Double
+    
+}
